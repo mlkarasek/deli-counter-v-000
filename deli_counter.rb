ec53037d63e07478f87904e6cd644224d1208584
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 23:51:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line 
+  return "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving(katz_deli)
+  puts "There is nobody waiting to be served!"
+
+end
