3b5e1ead0dbf19d5e5bf4ff7e6163cb5c17e4d6e
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 23:58:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 def line(katz_deli)
   if katz_deli.empty?
-  puts "The line is currently empty."
-end
+    puts "The line is currently empty."
+  else
+    announcement = "The line is currently:"
+    katz_deli.each_with_index do |name,index|
+      announcement << " #{index+1}. #{name}"
+    end
+    puts announcement
+  end
+
 
 def take_a_number(katz_deli, name)
 
