93340fd3d33eb57b3ade59576401b7c45e10ee38
Monica  Karasek <mlkarasek@gmail.com>
2017-10-19 00:01:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,8 @@
 def line(katz_deli)
-  if katz_deli.empty?
+  
     puts "The line is currently empty."
   else
-    announcement = "The line is currently:"
-    katz_deli.each_with_index do |name,index|
-      announcement << " #{index+1}. #{name}"
-    end
-    puts announcement
+  
   end
 
 
